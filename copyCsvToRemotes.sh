43b0f5b3b77a2de048c06c26f81136bb09d48c95
germansandovalqualis <129901633+germansandovalqualis@users.noreply.github.com>
2023-06-22 12:46:39 -0400

    remove carriage return

--- copyCsvToRemotes.sh
+++ copyCsvToRemotes.sh
@@ -1,5 +1,5 @@
 path="/home/jmeter/csv"
 hosts=(10.100.9.98 10.100.9.99 10.100.9.170 10.100.9.171 10.100.9.172 10.100.9.173 10.100.9.174 10.100.9.175 10.100.9.176 10.100.9.177)
 for host in "${hosts[@]}"; do
-	scp -r "$path" "$host":"$path"
+	scp -r "$path" "$host":"path"
 done
